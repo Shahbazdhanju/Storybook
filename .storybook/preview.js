de65c17511caa09cfd4d7d4a4914cee5d2014303
unknown <shahbaz@cmail.carleton.ca>
2020-11-09 11:30:50 -0500

    chakra and theming

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,22 @@
-
-import React from 'react';
-import {addDecorator} from '@storybook/react'
-import Center from '../src/components/Center/Center'
-
-addDecorator(story => <Center>{story()}</Center>)
+import React from "react";
+import { addDecorator } from "@storybook/react";
+import Center from "../src/components/Center/Center";
+import { ThemeProvider, theme, CSSReset, Box } from "@chakra-ui/core";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  options:{
+
+  }
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <Box m="4">
+        <Story />
+      </Box>
+    </ThemeProvider>
+  ),
+];
